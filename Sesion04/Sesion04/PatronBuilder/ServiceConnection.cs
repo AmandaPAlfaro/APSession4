481cf155e314d99a841a072e63f6905fe950614b
AmandaPAlfaro <amanda.perez.captiva@outlook.com>
2025-04-04 12:07:28 -0400

    session4

---
+++ Sesion04/Sesion04/PatronBuilder/ServiceConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion04.PatronBuilder;
+
+internal class ServiceConnection
+{
+}
---
+++ Sesion04/Sesion04/Program.cs
@@ -0,0 +1,30 @@
+﻿using Sesion04.PatronSingleton;
+
+Console.WriteLine("Patrones Creacionales");
+
+Console.WriteLine("Patron Singleton");
+Console.WriteLine(new string('-', 100));
+
+Console.WriteLine("Singleton Ingenuo");
+
+var clase = SingletonIngenuo.GetInstance();
+
+clase.ImprimeMensaje();
+
+Console.WriteLine("Singleton seguro para hilos");
+var claseSegura = ThreadSafeSingleton.GetInstance();
+claseSegura.ImprimeMensaje();
+
+Console.WriteLine("Singleton con carga temprana");
+var singletonTemprano = EagerSingleton.Instance;
+singletonTemprano.ImprimeMensaje();
+
+
+
+Console.WriteLine("Singleton con carga diferida(Lazy)");
+var singletonLazy = SingletonLazy.Instance;
+singletonLazy.ImprimeMensaje();
+
+
+
+Console.ReadLine();
